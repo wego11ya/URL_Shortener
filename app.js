b4f4c95b6398cc132039ef324fbfa6a67627ab5d
wego11ya <wego11ya@gmail.com>
2022-10-26 14:33:38 +0800

    Set index+result page and routes, set routes for creating new data for originalURL

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
+const URL = require("./models/URL");
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -22,10 +23,22 @@ db.once("open", () => {
 
 app.engine("hbs", exphbs({ default: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
-
+// setting body-parser
+app.use(express.urlencoded({ extended: true }));
 // 設定首頁路由
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/URL", (req, res) => {
+  const originalURL = req.body.originalURL;
+  URL.create({ originalURL })
+    .then(() => res.redirect("/result"))
+    .catch((error) => console.log(error));
+});
+
+app.get("/result", (req, res) => {
+  res.render("result");
 });
 
 // 設定 port 3000
