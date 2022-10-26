aeb76239f98ce7f6e3585cd8b644e90c7bc8a62d
wego11ya <wego11ya@gmail.com>
2022-10-26 10:57:14 +0800

    Import express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require("express");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -19,9 +20,12 @@ db.once("open", () => {
   console.log("mongodb connected!");
 });
 
+app.engine("hbs", exphbs({ default: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
 // 設定首頁路由
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("index");
 });
 
 // 設定 port 3000
