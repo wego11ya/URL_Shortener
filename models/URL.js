c156db05b9a837f676aa8f8c76ec46ef789acd63
wego11ya <wego11ya@gmail.com>
2022-10-26 10:30:56 +0800

    Import Mongoose and link to MongoDB

---
+++ models/URL.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const URLSchema = new Schema({
+  originalURL: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.model("URL", URLSchema);
